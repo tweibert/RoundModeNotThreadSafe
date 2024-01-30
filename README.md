ad4f282ee9421f9234a5e481804c360ccec3901b
Torben Weibert <tw@mairlist.com>
2024-01-30 17:08:19 +0100

    Added thread-safe implementation of SetRoundMode for comparison

--- README.md
+++ README.md
@@ -3,9 +3,19 @@ A simple demo project to show that Delphi's `SetRoundMode` is not thread-safe.
 Output:
 
 ````
+Delphi non-thread-safe SetRoundMode:
+
 MainThread init                def: 1332   current: 1372  RM: 0
 MainThread after rmDown        def: 1772   current: 1772  RM: 1
 Thread 00:59:59.999            def: 1772   current: 1772  RM: 1
+MainThread after rmNearest     def: 1372   current: 1372  RM: 0
+Thread 01:00:00.000            def: 1372   current: 1372  RM: 0
+
+Custom thread-safe SetRoundMode:
+
+MainThread init                def: 1372   current: 1372  RM: 0
+MainThread after rmDown        def: 1372   current: 1772  RM: 1
+Thread 01:00:00.000            def: 1372   current: 1372  RM: 0
 MainThread after rmNearest     def: 1372   current: 1372  RM: 0
 Thread 01:00:00.000            def: 1372   current: 1372  RM: 0
 ````
--- RoundModeNotThreadSafe.dpr
+++ RoundModeNotThreadSafe.dpr
@@ -10,6 +10,26 @@ uses
   System.DateUtils,
   System.Math;
 
+
+procedure Set8087CWThreadSafe(ANewCW: Word);
+var
+  L8087CW: Word;
+asm
+  mov L8087CW, ANewCW
+  fnclex
+  fldcw L8087CW
+end;
+
+function SetRoundModeThreadSafe(const RoundMode: TRoundingMode): TRoundingMode;
+var
+  CtlWord: Word;
+begin
+  CtlWord := Get8087CW;
+  Set8087CWThreadSafe((CtlWord and $F3FF) or (Ord(RoundMode) shl 10));
+  Result := TFPURoundingMode((CtlWord shr 10) and 3);
+end;
+
+
 procedure PrintCW(iScope: string);
 begin
   WriteLn(Format('%-30s def: %.4x   current: %.4x  RM: %d', [iScope, Default8087CW, Get8087CW, ord(GetRoundMode)]));
@@ -21,17 +41,43 @@ begin
 end;
 
 begin
-  PrintCW('MainThread init');
-
   try
+    WriteLn('Delphi non-thread-safe SetRoundMode:');
+    WriteLn;
+
+    PrintCW('MainThread init');
+
     // Alter Rounding mode, run a background thread, change it back
     SetRoundMode(rmDown);
     try
       PrintCW('MainThread after rmDown');
       TThread.CreateAnonymousThread(ThreadProc).Start;
-      Sleep(1000);
+      Sleep(500);
     finally
       SetRoundMode(rmNearest);
+    end;
+
+    // Run another thread
+    PrintCW('MainThread after rmNearest');
+
+    TThread.CreateAnonymousThread(ThreadProc).Start;
+    Sleep(500);
+
+    // Now do everything again with a thread-safe version of SetRoundMode
+    WriteLn;
+    WriteLn('Custom thread-safe SetRoundMode:');
+    WriteLn;
+
+    PrintCW('MainThread init');
+
+    // Alter Rounding mode, run a background thread, change it back
+    SetRoundModeThreadSafe(rmDown);
+    try
+      PrintCW('MainThread after rmDown');
+      TThread.CreateAnonymousThread(ThreadProc).Start;
+      Sleep(500);
+    finally
+      SetRoundModeThreadSafe(rmNearest);
     end;
 
     // Run another thread
